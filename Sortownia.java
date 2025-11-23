3f5d273c059c106f705825633b1ff53ade3d16a5
Hubkas <ryska2019@gmail.com>
2025-11-23 20:33:58 +0100

    Delete Sortownia.java

--- Sortownia.java
+++
@@ -1,9 +0,0 @@
-public class Sortownia {
-    final int sortowniaX;
-    final int sortowniaY;
-    public Sortownia(int  sortowniaX, int sortowniaY) {
-        this.sortowniaX = sortowniaX;
-        this.sortowniaY = sortowniaY;
-    }
-    static final Sortownia SORTOWNIA= new Sortownia(0,0);
-}
